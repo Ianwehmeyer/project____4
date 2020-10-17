4b7cdd65f93547739f02f2f64d1434f9e95582cf
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-10-17 15:15:34 -0400

    Decimal Field
    Expirament with Decimal Field

--- stockapp/portfolio/forms.py
+++ stockapp/portfolio/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class CalcForm( forms.Form):
-    amount = forms.DecimalField(initial=10, max_digits=1000000, decimal_places=10) #can I pass database amount to there?
+    amount = forms.DecimalField(initial=10, max_digits=20, decimal_places=2) #can I pass database amount to there?
--- stockapp/portfolio/models.py
+++ stockapp/portfolio/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Account( models.Model):
-    balance = models. DecimalField(max_digits=1000000, decimal_places=2);
+    balance = models. DecimalField(max_digits=20, decimal_places=2);
     #NOTE FOR SELF - balance will act as the amount of cash funds in User's account
 
 
 class Things( models.Model): #figure out what we're supposed to do with this
-    amount = models.DecimalField #values that should be decimal
+    amount = models.DecimalField(max_digits=20, decimal_places=2); #values that should be decimal
+    name = models.CharField( max_length=30)
