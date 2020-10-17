6dc70a44492a14fef63b437c9a97281e2d278d18
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-10-17 12:06:01 -0400

    def home created
    Created home function

--- stockapp/portfolio/views.py
+++ stockapp/portfolio/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+def home(request):
+    context = { 'username': 'Ian Studmeyer'}
+    return render( request, 'example/home.html', context)
