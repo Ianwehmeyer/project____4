c91deed5f3a558144557d96378344500ea43545f
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-10-17 12:30:05 -0400

    Created Class "balance"
    balance will act as user's amount of funds available to purchase or sell stock

--- stockapp/portfolio/models.py
+++ stockapp/portfolio/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Account( models,Model):
+    balance = models. IntegerField();
+    #NOTE FOR SELF - balance will act as the amount of cash funds in User's account
--- stockapp/portfolio/views.py
+++ stockapp/portfolio/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 def home(request):
     context = { 'username': 'Ian Studmeyer'}
-    return render( request, 'example/home.html', context)
+    return render( request, 'portfolio/home.html', context)
