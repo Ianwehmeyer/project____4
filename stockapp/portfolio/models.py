b35a9fcee4b624e34884cd95ea4b798971914ced
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-10-17 14:50:51 -0400

    Add + Subtract
    Need to work on making them work as decimals

--- stockapp/portfolio/models.py
+++ stockapp/portfolio/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Account( models.Model):
-    balance = models. IntegerField();
+    balance = models. DecimalField(max_digits=1000000, decimal_places=2);
     #NOTE FOR SELF - balance will act as the amount of cash funds in User's account
+
+
+class Things( models.Model): #figure out what we're supposed to do with this
+    amount = models.DecimalField #values that should be decimal
--- stockapp/portfolio/views.py
+++ stockapp/portfolio/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CalcForm
+from .models import Account
 
 # Create your views here.
 def home(request):
-    context = { 'username': 'Ian Studmeyer'}
+    account = get_object_or_404( Account, pk=1)
+    balance = account.balance
+    context = { 'username': 'Ian Studmeyer', 'balance' : balance}
     return render( request, 'portfolio/home.html', context)
 
 def calc(request):
+    account = get_object_or_404( Account, pk=1)
+    balance = account.balance
+    if request.method == "POST":
+        form = CalcForm( request.POST)
+        if form.is_valid(): #look up error codes for is_valid option if else
+            amount = form.cleaned_data['amount']
+            action = request.POST['submit'] 
+            if action == "Add": #note - will want to change to buy
+                account.balance = balance + amount
+            else:
+                if amount <= balance:
+                    account.balance = balance - amount
+                else:
+                    print('error: insufficient balance')
+            account.save();
+            return redirect('/')
     if request.method == "GET":
         form = CalcForm()
         context = { 'form': form}
