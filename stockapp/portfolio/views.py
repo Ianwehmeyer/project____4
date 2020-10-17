74648718ac883988ad9387e535938f6414acb837
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-10-17 13:46:31 -0400

    Calc continued
    Added more to Calc - will innevitably change name of function / html page
    
    Likely need to do something that will create split screen + proper ajax call + yfinance

--- stockapp/portfolio/views.py
+++ stockapp/portfolio/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CalcForm
 
 # Create your views here.
 def home(request):
     context = { 'username': 'Ian Studmeyer'}
     return render( request, 'portfolio/home.html', context)
+
+def calcome(request):
+    if request.method == "GET":
+        form = CalcForm()
+        context = { 'form': form}
+        return render( request, 'portfolio/calc.html', context)
